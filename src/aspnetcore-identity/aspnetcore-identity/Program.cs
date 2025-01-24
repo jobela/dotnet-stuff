8123ac0c331c0617fe84d0af04e85b053e4f160c
jobela <joar.langaard@gmail.com>
2025-01-24 21:27:22 +0100

    Update Program.cs

--- src/aspnetcore-identity/aspnetcore-identity/Program.cs
+++ src/aspnetcore-identity/aspnetcore-identity/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Hosting.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddAuthorization();
@@ -10,11 +9,9 @@ builder.Services.AddAuthentication();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
